b2a6cb2c1b02dcd43428ddf2d7b9930635681dbc
Andrew Rushworth <andrew.rushworth@smartbear.com>
2025-06-23 11:11:54 +0100

    Add prod support

--- src/app/aeronauts/page.tsx
+++ src/app/aeronauts/page.tsx
@@ -1,5 +1,8 @@
 import { getStoryblokApi } from '@/lib/storyblok';
 import { StoryblokStory } from '@storyblok/react/rsc';
+
+const version = (process.env.NEXT_PUBLIC_STORYBLOK_VERSION ?? 'draft') as 'draft' | 'published';
+
 export default async function Home() {
 	const { data } = await fetchData();
 	return (
@@ -10,5 +13,5 @@ export default async function Home() {
 }
  async function fetchData() {
 	const storyblokApi = getStoryblokApi();
-	return await storyblokApi.get(`cdn/stories/aeronauts`, { version: 'draft' });
+	return await storyblokApi.get(`cdn/stories/aeronauts`, { version: version });
 }
